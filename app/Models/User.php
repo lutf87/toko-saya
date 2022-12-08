029df53fa1fa34971a0b117c79f28b0941ed2504
lutf87 <luthfiarrahman007@gmail.com>
2022-12-08 15:48:51 +0700

    Setting role user

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'alamat',
+        'role',
+        'foto',
+        'status'
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,16 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone');
+            $table->text('alamat')->nullable();
+            $table->string('role')->default('member');
+            $table->string('foto')->nullable();
+            $table->string('status')->default('aktif');
             $table->rememberToken();
             $table->timestamps();
         });
